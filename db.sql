04feb193008dd3fb22209a52fc82408f8abc0a3d
gongcy <gongcy126@126.com>
2016-09-26 19:09:08 +0800

    add sql

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@ create database db_reglog;
 use db_reglog;
 
 create table tb_member(
-    id int(4) primary key auto_increment,
+  id int(4) primary key auto_increment,
 	name varchar(100) not null,
 	password varchar(200) not null,
 	answer varchar(100) not null,
@@ -13,6 +13,10 @@ create table tb_member(
 	birthday date not null,
 	telephone varchar(20) not null,
 	qq varchar(15),
-	count int(1),
-	active int(1)
+	count int(1) default 0,
+	active int(1) default 0
 )
+
+
+
+
